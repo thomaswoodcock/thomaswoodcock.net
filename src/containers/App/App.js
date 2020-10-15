a7009ae5e25e33f6d43a09784c5685a2a89c802a
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-15 13:22:37 +0100

    Adds Home

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,37 +2,30 @@ import React, { lazy, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import SkipLink from "../../components/SkipLink";
 import Spinner from "../../components/Spinner";
 
 import styles from "./App.module.css";
 
+const Home = lazy(() => import("../../pages/Home"));
 const NotFound = lazy(() => import("../../pages/NotFound"));
 
 const App = () => (
   <div className={styles.App}>
     <Suspense fallback={<Spinner />}>
       <Switch>
-        <Route exact path="/404">
-          <NotFound />
-        </Route>
+        <Route component={NotFound} exact path="/404" />
         <Route>
           <SkipLink />
           <NavBar links={[]} />
           <main id="main">
-            <Switch>
-              <Route exact path="/">
-                <Header
-                  subtitle="This is my personal website project."
-                  title="Thomas Woodcock"
-                />
-              </Route>
-              <Route>
-                <Redirect to="/404" />
-              </Route>
-            </Switch>
+            <Suspense fallback={<Spinner />}>
+              <Switch>
+                <Route component={Home} exact path="/" />
+                <Route component={() => <Redirect to="/404" />} />
+              </Switch>
+            </Suspense>
           </main>
           <Footer />
         </Route>
