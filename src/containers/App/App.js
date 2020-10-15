8fe55db6678c1b13a976c91d6dc822e2c0e2f6c8
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-15 08:05:49 +0100

    Adds NavBar test

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
@@ -11,33 +11,31 @@ import styles from "./App.module.css";
 import NotFound from "../../pages/NotFound";
 
 const App = () => (
-  <HashRouter basename="/">
-    <div className={styles.App}>
-      <Switch>
-        <Route exact path="/404">
-          <NotFound />
-        </Route>
-        <Route>
-          <SkipLink />
-          <NavBar links={[]} />
-          <main id="main">
-            <Switch>
-              <Route exact path="/">
-                <Header
-                  subtitle="This is my personal website project."
-                  title="Thomas Woodcock"
-                />
-              </Route>
-              <Route>
-                <Redirect to="/404" />
-              </Route>
-            </Switch>
-          </main>
-          <Footer />
-        </Route>
-      </Switch>
-    </div>
-  </HashRouter>
+  <div className={styles.App}>
+    <Switch>
+      <Route exact path="/404">
+        <NotFound />
+      </Route>
+      <Route>
+        <SkipLink />
+        <NavBar links={[]} />
+        <main id="main">
+          <Switch>
+            <Route exact path="/">
+              <Header
+                subtitle="This is my personal website project."
+                title="Thomas Woodcock"
+              />
+            </Route>
+            <Route>
+              <Redirect to="/404" />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </Route>
+    </Switch>
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.css";
 
@@ -7,7 +8,9 @@ import App from "./containers/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
