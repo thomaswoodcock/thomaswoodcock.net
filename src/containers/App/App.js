3a0c37f6055329243b1bc78982604b9c9a8e741f
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-15 12:52:40 +0100

    Adds Spinner

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import SkipLink from "../../components/SkipLink";
+import Spinner from "../../components/Spinner";
 
 import styles from "./App.module.css";
 
-import NotFound from "../../pages/NotFound";
+const NotFound = lazy(() => import("../../pages/NotFound"));
 
 const App = () => (
   <div className={styles.App}>
-    <Switch>
-      <Route exact path="/404">
-        <NotFound />
-      </Route>
-      <Route>
-        <SkipLink />
-        <NavBar links={[]} />
-        <main id="main">
-          <Switch>
-            <Route exact path="/">
-              <Header
-                subtitle="This is my personal website project."
-                title="Thomas Woodcock"
-              />
-            </Route>
-            <Route>
-              <Redirect to="/404" />
-            </Route>
-          </Switch>
-        </main>
-        <Footer />
-      </Route>
-    </Switch>
+    <Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route exact path="/404">
+          <NotFound />
+        </Route>
+        <Route>
+          <SkipLink />
+          <NavBar links={[]} />
+          <main id="main">
+            <Switch>
+              <Route exact path="/">
+                <Header
+                  subtitle="This is my personal website project."
+                  title="Thomas Woodcock"
+                />
+              </Route>
+              <Route>
+                <Redirect to="/404" />
+              </Route>
+            </Switch>
+          </main>
+          <Footer />
+        </Route>
+      </Switch>
+    </Suspense>
   </div>
 );
 
