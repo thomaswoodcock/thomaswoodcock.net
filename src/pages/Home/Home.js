8f9564efc9d8de730958e8596a7c19dad1c0d0ef
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-15 13:25:17 +0100

    Adds Home (#15)

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Header from "../../components/Header";
+
+const Home = () => (
+  <Header
+    subtitle="This is my personal website project."
+    title="Thomas Woodcock"
+  />
+);
+
+export default Home;
