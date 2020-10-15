2bb4728eeea117ff7d2177ba5daa086f88896523
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-15 19:18:48 +0100

    Adds message on homepage

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Spinner.module.css";
 
 const Spinner = () => (
-  <div className={styles.Spinner}>
+  <div className={styles.Spinner} role="progressbar">
     <svg viewBox="0 0 100 100">
       <circle cx="50" cy="50" r="42" />
     </svg>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import Container from "../../components/Container";
 import Header from "../../components/Header";
+import Heading from "../../components/Heading";
+import Paragraph from "../../components/Paragraph";
 
 const Home = () => (
-  <Header
-    subtitle="This is my personal website project."
-    title="Thomas Woodcock"
-  />
+  <>
+    <Header
+      subtitle="This is my personal website project."
+      title="Thomas Woodcock"
+    />
+    <Container>
+      <article>
+        <Heading level={2}>Hello</Heading>
+        <Paragraph>
+          {"I'm Tom, I'm a web developer, and this is my personal website. "}
+        </Paragraph>
+        <Paragraph>
+          {`There's not much here, yet, but eventually, this is where I'll show
+          and discuss the projects that I'm working on.`}
+        </Paragraph>
+        <Paragraph>
+          {`In the meantime, this website is my project. Come back every now
+          and then to see how it's progressing!`}
+        </Paragraph>
+      </article>
+    </Container>
+  </>
 );
 
 export default Home;
