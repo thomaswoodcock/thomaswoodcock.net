d71a3570c4be03283dc3a58b23b6f1c71530dcd6
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-16 06:59:42 +0100

    Adds test for default route

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -6,7 +6,24 @@ import { Router } from "react-router-dom";
 import App from "./App";
 
 describe("<App />", () => {
-  it("displays 404 page when given invalid route", async () => {
+  it("renders homepage by default", async () => {
+    // Arrange
+    const history = createMemoryHistory();
+
+    // Act
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+
+    // Assert
+    expect(
+      await screen.findByRole("heading", { name: "Thomas Woodcock" })
+    ).toBeVisible();
+  });
+
+  it("renders 404 page when given invalid route", async () => {
     // Arrange
     const history = createMemoryHistory();
     history.push("/invalid/route");
