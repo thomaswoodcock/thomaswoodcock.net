8696ad676a1ea175cf2ad9cd5003aa34b3f19327
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-18 08:19:35 +0100

    Makes NavBar collapsible (#21)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,13 +3,18 @@ import PropTypes from "prop-types";
 
 import styles from "./Button.module.css";
 
-const Button = ({ children, onClick }) => (
-  <button className={styles.Button} onClick={onClick}>
+const Button = ({ ariaExpanded = false, children, onClick }) => (
+  <button
+    aria-expanded={ariaExpanded}
+    className={styles.Button}
+    onClick={onClick}
+  >
     {children}
   </button>
 );
 
 Button.propTypes = {
+  ariaExpanded: PropTypes.bool,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired,
 };
---
+++ src/containers/NavBar/NavBar.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Link, useLocation } from "react-router-dom";
+
+import Button from "../../components/Button";
+
+import styles from "./NavBar.module.css";
+
+const NavBar = ({ links = [] }) => {
+  const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpanded(false);
+  }, [location]);
+
+  return (
+    <nav className={styles.NavBar}>
+      <Link to="/">
+        Thomas Woodcock <span className="hidden">Home</span>
+      </Link>
+      {links.length > 0 && (
+        <>
+          <Button
+            ariaExpanded={expanded}
+            onClick={() => setExpanded((exp) => !exp)}
+          >
+            <svg viewBox="0 0 100 100">
+              <path
+                d="M3,7 95,7 M3,50 95,50 M3,92 93,93"
+                fill="none"
+                focusable="false"
+                stroke="currentColor"
+                strokeWidth="15"
+              />
+            </svg>
+            Menu
+          </Button>
+          <ul>
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link to={link.url}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </nav>
+  );
+};
+
+NavBar.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default NavBar;
