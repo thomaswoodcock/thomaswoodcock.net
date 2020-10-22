cb386d57ed4550baad49eb35da4356bc62555f41
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-22 07:22:05 +0100

    Removes Paragraph

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "../../components/Header";
 import Heading from "../../components/Heading";
-import Paragraph from "../../components/Paragraph";
 
 import styles from "./Home.module.css";
 
@@ -13,19 +12,19 @@ const Home = () => (
       title="Thomas Woodcock"
     />
     <div className={styles.container}>
-      <article>
+      <article className={styles.content}>
         <Heading level={2}>Hello</Heading>
-        <Paragraph>
+        <p>
           {"I'm Tom, I'm a web developer, and this is my personal website. "}
-        </Paragraph>
-        <Paragraph>
+        </p>
+        <p>
           {`There's not much here, yet, but eventually this is where I'll show
           and discuss the projects that I'm working on.`}
-        </Paragraph>
-        <Paragraph>
+        </p>
+        <p>
           {`In the meantime, this website is my project. Come back every now
           and then to see how it's progressing!`}
-        </Paragraph>
+        </p>
       </article>
     </div>
   </>
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Heading from "../../components/Heading";
-import Paragraph from "../../components/Paragraph";
 
 import styles from "./NotFound.module.css";
 
@@ -10,9 +9,9 @@ const NotFound = () => (
   <div className={styles.root}>
     <Heading level={1}>404</Heading>
     <Heading level={2}>Page not found.</Heading>
-    <Paragraph>
+    <p className={styles.link}>
       <Link to="/">Back to thomaswoodcock.net</Link>
-    </Paragraph>
+    </p>
   </div>
 );
 
