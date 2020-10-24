6d78250d420f39799fd041b9a7d29f1273a7f3b7
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-24 16:04:59 +0100

    Adds theme

---
+++ src/themes/default.js
@@ -0,0 +1,28 @@
+const COLOR_DARK = "#252525";
+const COLOR_LIGHT = "#F5F5F5";
+
+export default {
+  colors: {
+    background: {
+      primary: COLOR_LIGHT,
+      secondary: COLOR_DARK,
+    },
+    border: {
+      primary: COLOR_DARK,
+      secondary: COLOR_LIGHT,
+    },
+    typography: {
+      primary: COLOR_DARK,
+      secondary: COLOR_LIGHT,
+    },
+  },
+  sizing: {
+    ratio: 1.4,
+    unit: "1rem",
+  },
+  typography: {
+    sans: '"Lato", Arial, Helvetica, sans-serif',
+    serif: '"Lora", "Palatino Linotype", "Book Antiqua", Palatino, serif',
+    special: '"Exo", Arial, Helvetica, sans-serif',
+  },
+};
