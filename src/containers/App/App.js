b6cb3591ef03441cea79f8f34a35264a78467cec
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-25 09:47:06 +0000

    Adds ThemeProvider

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from "react";
+import { ThemeProvider } from "emotion-theming";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import ErrorBoundary from "../../components/ErrorBoundary";
@@ -7,34 +8,39 @@ import NavBar from "../NavBar";
 import SkipLink from "../../components/SkipLink";
 import Spinner from "../../components/Spinner";
 
+import { createTheme } from "../../styles";
 import styles from "./App.module.css";
+
+const theme = createTheme();
 
 const Home = lazy(() => import("../../pages/Home"));
 const NotFound = lazy(() => import("../../pages/NotFound"));
 
 const App = () => (
-  <div className={styles.root}>
-    <ErrorBoundary>
-      <Suspense fallback={<Spinner />}>
-        <Switch>
-          <Route component={NotFound} exact path="/404" />
-          <Route>
-            <SkipLink />
-            <NavBar links={[]} />
-            <main className={styles.main} id="main">
-              <Suspense fallback={<Spinner />}>
-                <Switch>
-                  <Route component={Home} exact path="/" />
-                  <Route component={() => <Redirect to="/404" />} />
-                </Switch>
-              </Suspense>
-            </main>
-            <Footer />
-          </Route>
-        </Switch>
-      </Suspense>
-    </ErrorBoundary>
-  </div>
+  <ThemeProvider theme={theme}>
+    <div className={styles.root}>
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route component={NotFound} exact path="/404" />
+            <Route>
+              <SkipLink />
+              <NavBar links={[]} />
+              <main className={styles.main} id="main">
+                <Suspense fallback={<Spinner />}>
+                  <Switch>
+                    <Route component={Home} exact path="/" />
+                    <Route component={() => <Redirect to="/404" />} />
+                  </Switch>
+                </Suspense>
+              </main>
+              <Footer />
+            </Route>
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  </ThemeProvider>
 );
 
 export default App;
