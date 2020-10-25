b044d47058f1d3f677c9a4936ea450103ece61e8
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-10-25 16:24:48 +0000

    Adjust styling (#30)
    * Conditionally retrieves theme
    
    * Fixes and tests forwarding refs with withStyles

--- src/styles/createStyles/createStyles.js
+++ src/styles/createStyles/createStyles.js
@@ -7,10 +7,15 @@ const createStyles = (style) => () => {
   const emotionTheme = useTheme();
 
   const styles = useMemo(() => {
-    const theme =
-      (Object.keys(emotionTheme).length === 0 && createTheme()) || emotionTheme;
+    if (typeof style === "function") {
+      const theme =
+        (Object.keys(emotionTheme).length === 0 && createTheme()) ||
+        emotionTheme;
 
-    return typeof style === "function" ? style(theme) : style;
+      return style(theme);
+    }
+
+    return style;
   }, [emotionTheme]);
 
   return styles;
