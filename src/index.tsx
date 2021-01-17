c62e0e0b43e43b16dbf3dd4cf4be04ebed956b98
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-01-17 15:21:42 +0000

    Fixes formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import {StrictMode} from "react";
-import {render} from "react-dom";
+import { StrictMode } from "react";
+import { render } from "react-dom";
 import { HashRouter } from "react-router-dom";
 
 import "./index.css";
